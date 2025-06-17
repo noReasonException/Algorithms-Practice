6886652947af5182dfa7f63cc340fe3766f3e8ad
Stefanos Stefanou <stefanos.stefanou@century.tech>
2025-06-17 13:44:35 +0100

    l13-lecture-exersises

---
+++ Problems/Informal/Lecture-Exersises/L13/perm.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+#define ll long long
+//forward refs
+template <typename container> void cinfoCon(container& genericSequence,string id="None", int depth=0);
+
+//debug utils
+#ifdef DEBUG
+	#define cinfo cout
+	#define add <<
+	template <typename container> void cinfoCon(container& genericSequence,string id, int depth){
+		cout<<"=============cinfoCon Debug ("<<id<<") START =============\n";
+		string prefix = "";
+		for(int i=0;i<depth;i++) prefix+="\t";
+
+		for(auto every: genericSequence){
+			cout<<prefix<<every<<"\n";
+		}
+		cout<<"=============cinfoCon Debug ("<<id<<") END =============\n";
+
+	}
+#endif
+#ifndef DEBUG
+	#define cinfo ;
+	#define add ;
+	template <typename container> void cinfoCon(container& genericSequence,string id, int depth){
+		return ;
+	}
+
+#endif
+
+/**
+ 	*
+	? Stuff to look for ->
+    * stay organised
+    * int overflows, array bounds, etc.
+    * special cases (n=1)?
+    * do something instead of nothing
+    * timebox your approach
+    * simple is better than complex
+    * n % mod = (n % mod + mod) % mod;
+    * long long instead of int
+    
+*/
+//Given an integer 1 <= n <= 10, write a recursive algorithm that prints all the permutations of the set {1,2,3,...,n}.
+void solve(ll l){
+    
+}
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+
+	
+    
+	int n;
+	cin>>n;
+    solve(n);
+	return 0;
+}
