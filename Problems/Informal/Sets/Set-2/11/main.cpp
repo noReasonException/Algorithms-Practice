07f173fce199a16f5532551e0713e57112a94630
Stefanos Stefanou <stefanos.stefanou@century.tech>
2025-10-04 13:53:12 +0100

    feat: Set 2 Problem 11

---
+++ Problems/Informal/Sets/Set-2/11/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <assert.h>
+using namespace std;
+/**
+ *	Why?
+*/
+void solve(){
+	long long n;
+	cin >> n;
+	long long expected = (n*(n+1)/2) ;
+	long long sum = 0;
+	long long tmp = 0;
+	for(int i=0;i<n-1;i++){
+		cin>>tmp;
+		sum+=tmp;
+	}
+	cout<<expected-sum;
+}
+ 
+int main(){
+	solve();
+	cout<<"\n";
+	
+}
