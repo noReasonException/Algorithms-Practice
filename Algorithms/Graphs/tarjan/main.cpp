9a274b739454fb649106ef7cc11aaab7949d332d
Stefanos Stefanou <stefanos.stefanou@century.tech>
2025-11-28 12:37:44 +0000

    feat: passes SPOJ

--- Algorithms/Graphs/tarjan/main.cpp
+++ Algorithms/Graphs/tarjan/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <set>
+#include <algorithm>
 using namespace std;
 #define ll long long
 
@@ -42,51 +43,47 @@ template <typename container> void debug(container& genericSequence,string id="N
     
 */
 
-vector<vector<int>> adj;
+vector<vector<ll>> adj;
 vector<bool> visited;  
-vector<int> pre;
-vector<int> low;
-vector<int> parent;
-int order=0;
-int root = 0;
+vector<ll> pre;
+vector<ll> low;
+vector<ll> parent;
+vector<pair<ll,ll>> bridges;
+ll order=0;
+ll root = 0;
 
 
 void tarjan(int curr){
-    cout<<curr+1<<" -> ";
     visited[curr]=true;
     pre[curr]=order++;
     low[curr] = pre[curr];
     for (auto &x:adj[curr]) {
         
-        if(!visited[x]) {
-            //Span edge
+        if(!visited[x]) { //Span edge
             parent[x]=curr;
             tarjan(x);
             low[curr] = min(low[curr],low[x]);
         }
-        //visited
-        else{
-            //Not your parent
-            if(parent[curr]!=x){
+        
+        else{ //either parent OR back edge. 
+            
+            if(parent[curr]!=x){ //select only the span edge
                 low[curr] = min(low[curr],low[x]);
-            }
-            //Your parent
-            else{
-                // ignore;
             }
         }
     }
 }
 
-void solve(){
-	ll n,l,s,t;
-    cin>>n>>l;
-    adj = vector<vector<int>>(n);
-    visited = vector<bool>(n,false);
-    parent = vector<int>(n,-1);
-    pre = vector<int>(n,-1);
-    low = vector<int>(n,-1);
-    for (ll i = 0; i < l; i++)
+void solve(ll caso){
+	ll v,e,s,t;
+    cin>>v>>e;
+    adj = vector<vector<ll>>(v);
+    visited = vector<bool>(v,false);
+    parent = vector<ll>(v,-1);
+    pre = vector<ll>(v,-1);
+    low = vector<ll>(v,-1);
+    bridges = vector<pair<ll,ll>>();
+    for (ll i = 0; i < e; i++)
     {
         cin>>s>>t;
         s--;t--;
@@ -94,28 +91,32 @@ void solve(){
         adj[t].push_back(s);
     }
     
-    cout<<"\n";
     parent[root]=root;
     tarjan(root);
-    cout<<"\n\n";
-    cout<<"Node\tPre\tLow\tLow points to node\n";
-    for(int i=0;i<n;i++){
-        const auto found = find(pre.begin(),pre.end(),low[i]);
-        cout<<i+1<<"\t"<<pre[i]<<"\t"<<low[i]<<"\t"<<*found + 1<<"\n";
-        cout<<"----------------------------------------------\n";
-    }
-    cout<<"\n";
-    cout<<"Node\tParent\t\n";
-    for(int i=0;i<n;i++){
-        cout<<i+1<<"\t"<<parent[i]+1<<"\t\n";
-    }
-    //Bridge Detection
-    cout<<"\n";
-    for(int s=0;s<n;s++){
+    ll lo,hi;
+    for(ll s=0;s<v;s++){
         for(auto &t:adj[s]){
-            if(pre[s]<low[t]) cout<<"Bridge\t"<<s+1<<"\t"<<t+1<<"\t\n";            
+            if(pre[s]<low[t]) {
+                lo=min(s,t);
+                hi=max(s,t);
+                bridges.push_back(std::make_pair(lo,hi));
+            }
         }
     }
+    sort(bridges.begin(),bridges.end());
+    cout<<"Caso #"<<caso<<"\n";
+    if(bridges.empty()){
+        cout<<"Sin bloqueos\n";
+    }
+    else{  
+        cout<<bridges.size()<<"\n";
+        for(auto each:bridges){
+
+            cout<<each.first+1<<" "<<each.second+1<<'\n';
+        }
+    }
+    
+    
     
 
     
@@ -127,9 +128,14 @@ int main(){
 	cin.tie(0);
 	//freopen("input.txt", "r", stdin);
 	//freopen("output.txt", "w", stdout);
-
+    ll t;
+    cin>>t;
+    for (ll i = 0; i < t; i++)
+    {
+        solve(i+1);
+    }
 	
 
-	solve();
+	
 	return 0 ;
 }
